324a88620ca537d64c4b1d03e26a426fb4b0b271
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-04-03 14:02:54 -0300

    Requisito 8 - Linhas da tabela de despesas

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -8,36 +8,38 @@ class Expenses extends React.Component {
     return (
       <section>
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          {expenses.map((e, key) => (
+            <tr key={ key }>
+              <td>{ e.description }</td>
+              <td>{ e.tag }</td>
+              <td>{ e.method }</td>
+              <td>{ Number(e.value).toFixed(2)}</td>
+              <td>{ e.exchangeRates[e.currency].name.split('/Real Brasileiro') }</td>
+              <td>{ Number(e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+              <td>
+                {(Number(e.value) * Number(e.exchangeRates[e.currency].ask)).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          ))}
         </table>
-        {/* {expenses.map((expense, key) => (
-          <div key={ key } className="expense">
-            <p>{ expense.description }</p>
-            <p>{ expense.tag }</p>
-            <p>{ expense.method }</p>
-            <p>{ expense.value }</p>
-            <p>{ expense.currency }</p>
-            <p>{ expense.exchangeRates[expense.currency].name }</p>
-            <p>
-              { Number(expense.value) * Number(expense.exchangeRates[expense.currency].ask) }
-            </p>
-            <p>{ expense.exchangeRates[expense.currency].codein }</p>
-            <div className="buttons">
-              <button type="button">Editar</button>
-              <button type="button">Excluir</button>
-            </div>
-          </div>
-        ))} */}
       </section>
     );
   }
